e4bfa3c0ed66b8284dfe97ed90e69bd186e03588
addvp <153482041+addvp@users.noreply.github.com>
2023-12-28 14:27:32 +0000

    done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+// Declaring variable in different methods
+const accountId = 223344
+let accountEmail ="ad@gmail.com"
+var accountPassword = "12345"
+let accountState;
+accountCity ="Delhi"
+
+
+accountEmail = "abc@gmail.com"
+accountPassword ="abc1234"
+accountCity ="Mumbai"
+
+//To show output 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
