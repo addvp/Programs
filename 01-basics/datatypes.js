85323fbeda035a9a99b217a54bff3d396167f1f0
addvp <153482041+addvp@users.noreply.github.com>
2023-12-29 06:42:05 +0000

    done

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,24 @@
+// **Types of Datatypes primitive and Non-Primitive**
+
+/* -- Primitive :  String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+-- Non primitive: Array, Objects, Functions */
+
+// Primitive
+const score = 100
+const name ="addv"
+const isLoggedIn = false
+const id = Symbol("123") 
+
+//Non-Primitive
+const carBrands = ["Honda","BMW","Audi"]
+
+let myObj = {
+    fname= "adhi",
+    Lname = "dev",
+    age = 22,
+}
+
+const myfun = function () {
+    console.log("Hello worlds");
+}
